db461d832ace5ff62cd43097233c2023644b0301
Glitch (invited-proximal-swim) <none>
2023-10-14 16:40:57 +0000

    📠🎩 Checkpoint

---
+++ www.shubouk.com/rss.php
@@ -0,0 +1,219 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom">
+  <channel>
+        <title>مقاول شبوك ونخيل 0537696910</title>
+        <link>https://www.shubouk.com/</link>
+        <description>مقاول شبوك ونخيل 0537696910</description>
+
+     <item>
+      <title><![CDATA[ مقاول ديكورات ومجالس تراثية | الرياض  ]]> </title>
+      <link>https://www.shubouk.com/post/1232/مقاول-ديكورات-ومجالس-تراثية-الرياض</link>
+      <guid>https://www.shubouk.com/post/1232/مقاول-ديكورات-ومجالس-تراثية-الرياض</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/966351661622240.JPG'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/966351661622240.JPG" lenght="51265" type="image/jpeg" />
+	   <pubDate>Sat, 27 Aug 2022 20:44:00 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Sat, 27 Aug 2022 20:44:00 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ توريد نخيل في الرياض | 0537696910 ]]> </title>
+      <link>https://www.shubouk.com/post/1231/توريد-نخيل-في-الرياض-0537696910</link>
+      <guid>https://www.shubouk.com/post/1231/توريد-نخيل-في-الرياض-0537696910</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/290461661621653.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/290461661621653.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Sat, 27 Aug 2022 20:34:13 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Sat, 27 Aug 2022 20:34:13 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ مقاول تركيب شبوك زراعية بالرياض ]]> </title>
+      <link>https://www.shubouk.com/post/1227/مقاول-تركيب-شبوك-زراعية-بالرياض</link>
+      <guid>https://www.shubouk.com/post/1227/مقاول-تركيب-شبوك-زراعية-بالرياض</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/452491661547485.jpeg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/452491661547485.jpeg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Fri, 26 Aug 2022 23:58:05 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Fri, 26 Aug 2022 23:58:05 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ ديكورات ومجالس تراثية | الرياض ]]> </title>
+      <link>https://www.shubouk.com/post/1209/ديكورات-ومجالس-تراثية-الرياض</link>
+      <guid>https://www.shubouk.com/post/1209/ديكورات-ومجالس-تراثية-الرياض</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/244581661284338.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/244581661284338.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Tue, 23 Aug 2022 22:52:18 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Tue, 23 Aug 2022 22:52:18 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ مقاول أعمال تراثية بالرياض 0537696910 ]]> </title>
+      <link>https://www.shubouk.com/post/1119/مقاول-أعمال-تراثية-بالرياض-0537696910</link>
+      <guid>https://www.shubouk.com/post/1119/مقاول-أعمال-تراثية-بالرياض-0537696910</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/914861660570607.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/914861660570607.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Mon, 15 Aug 2022 16:36:47 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Mon, 15 Aug 2022 16:36:47 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ مقاول توريد نخيل في الرياض ]]> </title>
+      <link>https://www.shubouk.com/post/1116/مقاول-توريد-نخيل-في-الرياض</link>
+      <guid>https://www.shubouk.com/post/1116/مقاول-توريد-نخيل-في-الرياض</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/844551660505955.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/844551660505955.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Sun, 14 Aug 2022 22:39:15 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Sun, 14 Aug 2022 22:39:15 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ مقاول بناء مجالس تراثية أسعار منافسة 0537696910 ]]> </title>
+      <link>https://www.shubouk.com/post/49/مقاول-بناء-مجالس-تراثية-أسعار-منافسة-0537696910</link>
+      <guid>https://www.shubouk.com/post/49/مقاول-بناء-مجالس-تراثية-أسعار-منافسة-0537696910</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/829251638020410.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/829251638020410.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Sat, 27 Nov 2021 16:40:10 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Sat, 27 Nov 2021 16:40:10 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ مقاول شبوك الرياض أسعار رخيصة 0537696910 ]]> </title>
+      <link>https://www.shubouk.com/post/48/مقاول-شبوك-الرياض-أسعار-رخيصة-0537696910</link>
+      <guid>https://www.shubouk.com/post/48/مقاول-شبوك-الرياض-أسعار-رخيصة-0537696910</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/716981638020239.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/716981638020239.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Sat, 27 Nov 2021 16:37:19 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Sat, 27 Nov 2021 16:37:19 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ مقاول نخيل الرياض 0537696910 ]]> </title>
+      <link>https://www.shubouk.com/post/46/مقاول-نخيل-الرياض-0537696910</link>
+      <guid>https://www.shubouk.com/post/46/مقاول-نخيل-الرياض-0537696910</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/305631637758977.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/305631637758977.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Wed, 24 Nov 2021 16:02:57 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Wed, 24 Nov 2021 16:02:57 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ أفضل مقاول شبوك بالرياض 0537696910 ]]> </title>
+      <link>https://www.shubouk.com/post/45/أفضل-مقاول-شبوك-بالرياض-0537696910</link>
+      <guid>https://www.shubouk.com/post/45/أفضل-مقاول-شبوك-بالرياض-0537696910</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/152121637758846.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/152121637758846.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Wed, 24 Nov 2021 16:00:46 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Wed, 24 Nov 2021 16:00:46 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ مجالس وديكورات تراثية | مؤسسة منزل البهجه بالرياض ]]> </title>
+      <link>https://www.shubouk.com/post/44/مجالس-وديكورات-تراثية-مؤسسة-منزل-البهجه-بالرياض</link>
+      <guid>https://www.shubouk.com/post/44/مجالس-وديكورات-تراثية-مؤسسة-منزل-البهجه-بالرياض</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/540981637579542.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/540981637579542.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Mon, 22 Nov 2021 14:12:22 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Mon, 22 Nov 2021 14:12:22 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ مقاول شبوك الرياض | مؤسسة منزل البهجه 0537696910 ]]> </title>
+      <link>https://www.shubouk.com/post/43/مقاول-شبوك-الرياض-مؤسسة-منزل-البهجه-0537696910</link>
+      <guid>https://www.shubouk.com/post/43/مقاول-شبوك-الرياض-مؤسسة-منزل-البهجه-0537696910</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/838161637579281.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/838161637579281.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Mon, 22 Nov 2021 14:08:01 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Mon, 22 Nov 2021 14:08:01 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ أفضل مؤسسة لتوريد أشجار النخيل الرياض 0537696910 ]]> </title>
+      <link>https://www.shubouk.com/post/42/أفضل-مؤسسة-لتوريد-أشجار-النخيل-الرياض-0537696910</link>
+      <guid>https://www.shubouk.com/post/42/أفضل-مؤسسة-لتوريد-أشجار-النخيل-الرياض-0537696910</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/168881637503170.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/168881637503170.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Sun, 21 Nov 2021 16:59:30 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Sun, 21 Nov 2021 16:59:30 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ أفضل مؤسسة شبوك بالرياض أسعار منافسة 0537696910 ]]> </title>
+      <link>https://www.shubouk.com/post/41/أفضل-مؤسسة-شبوك-بالرياض-أسعار-منافسة-0537696910</link>
+      <guid>https://www.shubouk.com/post/41/أفضل-مؤسسة-شبوك-بالرياض-أسعار-منافسة-0537696910</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/312891637503026.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/312891637503026.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Sun, 21 Nov 2021 16:57:06 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Sun, 21 Nov 2021 16:57:06 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+         <item>
+      <title><![CDATA[ مؤسسة ديكورات مجالس تراثية طينية بالرياض 0537696910 ]]> </title>
+      <link>https://www.shubouk.com/post/39/مؤسسة-ديكورات-مجالس-تراثية-طينية-بالرياض-0537696910</link>
+      <guid>https://www.shubouk.com/post/39/مؤسسة-ديكورات-مجالس-تراثية-طينية-بالرياض-0537696910</guid>
+     <description><![CDATA[ <img src='https://www.shubouk.com/files/780341637412630.jpg'>]]> </description>
+       <enclosure url="https://www.shubouk.com/files/780341637412630.jpg" lenght="51265" type="image/jpeg" />
+	   <pubDate>Sat, 20 Nov 2021 15:50:30 +0300</pubDate>
+	    <language>ar-sa</language>
+    <copyright>Copyright 2022 - www.shubouk.com</copyright>
+    <lastBuildDate>Sat, 20 Nov 2021 15:50:30 +0300</lastBuildDate>
+    <category>مظلات</category>
+    <generator>www.shubouk.com</generator>
+    <ttl>1440</ttl>
+    </item>
+      </channel>
+</rss>
